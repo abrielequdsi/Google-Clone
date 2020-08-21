4dc8d5aba17c77302b994932faedb2096f61be84
Abriele Qudsi <abrielebromozentia@gmail.com>
2020-08-21 18:27:24 +0700

    finish search component  in main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+import Search from "../components/Search";
 
 function Home() {
   return (
@@ -13,9 +16,19 @@ function Home() {
         <div class="home__headerRight">
           <Link to="/gmail">Gmail</Link>
           <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar />
         </div>
       </div>
-      <div class="home__body"></div>
+      <div class="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt=""
+        />
+        <div class="home__inputContainer">
+          <Search />
+        </div>
+      </div>
     </div>
   );
 }
